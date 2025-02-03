66f3643577c6d18d01fbad0f8324b65d77fa0e87
mohamedDDA <milanone4@gmail.com>
2025-02-03 12:38:04 +0200

    Initial commit

--- next.config.js
+++ next.config.js
@@ -25,6 +25,30 @@ const nextConfig = {
         source: '/en/robots.txt',  // or any other locale path
         destination: '/robots.txt', // redirect to the root robots.txt
       },
+      {
+        source: '/ar/favicon.ico',
+        destination: '/favicon.ico', // Map localized path to the root favicon
+      },
+      {
+        source: '/en/favicon.ico',
+        destination: '/favicon.ico', // Map localized path to the root favicon
+      },
+      {
+        source: '/ar/icon.svg',
+        destination: '/icon.svg', // Map localized path to the root SVG icon
+      },
+      {
+        source: '/en/icon.svg',
+        destination: '/icon.svg', // Map localized path to the root SVG icon
+      },
+      {
+        source: '/ar/apple-touch-icon.png',
+        destination: '/apple-touch-icon.png', // Map localized path to the root touch icon
+      },
+      {
+        source: '/en/apple-touch-icon.png',
+        destination: '/apple-touch-icon.png', // Map localized path to the root touch icon
+      },
     ];
   },
   reactStrictMode: true,
--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -79,6 +79,8 @@ export default function RootLayout({ children, params: { locale } }) {
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
 
